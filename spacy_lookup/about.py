42cdedd77933c653c2795d700992e677dbc55336
Marc Puig <marc.puig@gmail.com>
2018-08-13 13:22:41 +0200

    update version to 0.0.3

--- spacy_lookup/about.py
+++ spacy_lookup/about.py
@@ -1,5 +1,5 @@
 __title__ = 'spacy-lookup'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __summary__ = 'spaCy pipeline component for Named Entity Recognition based on dictionaries.'
 __url__ = 'https://github.com/mpuig/spacy-lookup'
 __author__ = 'Marc Puig'
